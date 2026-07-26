6f39f8c420707f9af88208b1b79393a7e759b7d9
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2026-07-26 08:07:13 -0300

    Desenvolvimento_Web_Joao_Ribeiro, 26/07/2026 (manhã)

---
+++ Secao_02_html5_css3_nivel_1/aula026_css3_box_model/index.php
@@ -0,0 +1,41 @@
+<?php
+
+include_once "conteudo.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CSS3 Box Model</title>
+    <!-- CSS -->
+    <link rel="stylesheet" href="estilos.css">
+</head>
+
+<body>
+    
+    <header>
+        <h1>
+            News Timing
+        </h1>
+    </header>
+
+    <article>
+        <h2>
+            Lorem ipsum dolor sit.
+        </h2>
+        <p>
+            <?= $texto ?>
+        </p>
+        <p>
+            <?= $texto ?>
+        </p>
+    </article>
+    
+</body>
+
+</html>
