79ccff80caa39296fc716e528f7e50e434658fcb
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2026-07-29 22:22:30 -0300

    Clube_Full_Stack 29/07/2026 (noite)

--- Secao_03_html5_intermediario_avancado/aula045_hiperligacoes/index2.php
+++ Secao_03_html5_intermediario_avancado/aula045_hiperligacoes/index2.php
@@ -5,14 +5,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title></title>
+    <title>Index 2</title>
     <!-- CSS -->
     
 </head>
 
 <body>
     
+    <header>
+        <h1>
+            Hiperligações
+        </h1>
+    </header>
     
+    <!-- NAV -->
+    <?php require "nav.php"; ?>
     
 </body>
 
