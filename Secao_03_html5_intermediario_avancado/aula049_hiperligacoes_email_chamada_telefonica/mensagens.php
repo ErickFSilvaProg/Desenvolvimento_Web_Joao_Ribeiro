c8e6f6165f68b1af11ae01dde4f77ac414238986
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2026-07-30 22:37:36 -0300

    Desenvolvimento_Web_Joao_Ribeiro, 30/07/2026 (noite)

---
+++ Secao_03_html5_intermediario_avancado/aula049_hiperligacoes_email_chamada_telefonica/mensagens.php
@@ -0,0 +1,7 @@
+<?php
+
+$mensagemZapp = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique quo alias est esse ullam sint, minima sed doloremque earum in, numquam iusto! Modi pariatur aperiam accusantium eveniet voluptas doloribus aliquam?";
+
+$tituloEmail = "Lorem ipsum dolor";
+
+$mensagemEmail = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique quo alias est esse ullam sint, minima sed doloremque earum in, numquam iusto!";
